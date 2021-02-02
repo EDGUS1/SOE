1657e491d441c842336accd0f54217cb6c26c422
EDGUS1 <eduardo.navarro2@unmsm.edu.pe>
2021-02-02 17:53:50 -0500

    subida con multer

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const multer = require("multer");
+const mimeTypes = require("mime-types");
+
 const user = require("../controllers/user");
 const homework = require("../controllers/homework");
 const course = require("../controllers/course");
@@ -8,6 +11,25 @@ const tarea = require('../controllers/tarea');
 const api = express.Router();
 const dbConnection = require("../connect");
 const connection = dbConnection();
+
+const storage = multer.diskStorage({
+    destination: "uploads/",
+    filename: function(req, file, cb) {
+        cb(
+            "",
+            Date.now() + file.originalname + "." + mimeTypes.extension(file.mimetype)
+        );
+    },
+});
+
+const upload = multer({
+    storage: storage,
+});
+
+
+api.post("/files", upload.single("file"), (req, res) => {
+    res.send("OK");
+});
 
 api.all("*", function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
