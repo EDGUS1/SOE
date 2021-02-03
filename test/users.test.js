b3ed8df2e581e492deefde09b8e17ccf5a266343
MarcosV333 <marcos.valdez333@hotmail.com>
2021-02-03 04:05:04 -0500

    sdasdsd

--- test/users.test.js
+++ test/users.test.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
+const { request } = require('express');
+const { nanoid } = require('nanoid');
 const Users = require('./users');
+const app= require('../routes/index');
+const reques=require ('supertest');
 
 jest.mock('axios');
 
@@ -14,3 +18,30 @@ test('Busca un usuario', () => {
   axios.get.mockResolvedValue(resp);
   return Users.all().then(data => expect(data).toEqual(users));
 });
+
+test('cantidad de caracteres de un id es igual a 36', () => {
+  const users = [{
+    "id": "12eb1ffb-90be-400b-b863-8f1bfdcc7376",
+    "nombre": "qwe",
+    "email": "qwe@gmail.com",
+    "contrasenia": "$2b$10$C/jqpTuJeGGdc2VUqR08NuJ/4HNKIwHbRfyyY4QL84RuOX6GRnvi."  
+  }];
+  
+  expect("12eb1ffb-90be-400b-b863-8f1bfdcc7376").toMatch("-");
+
+});
+
+test('Comprobacion de dirección de email', () => {
+  const users = [{
+    "id": "12eb1ffb-90be-400b-b863-8f1bfdcc7376",
+    "nombre": "qwe",
+    "email": "qwe@gmail.com",
+    "contrasenia": "$2b$10$C/jqpTuJeGGdc2VUqR08NuJ/4HNKIwHbRfyyY4QL84RuOX6GRnvi."  
+  }];
+  expect("qwe@gmail.com").toMatch(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/);
+
+});
+
+
+
+
